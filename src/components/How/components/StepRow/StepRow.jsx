335f904badcc5995bac78f9b04d1d5b930fbf313
Gordon <sgz886@gmail.com>
2023-08-25 20:43:49 +1000

    feat: finish How it works animation by Intersection Observer API

--- src/components/How/components/StepRow/StepRow.jsx
+++ src/components/How/components/StepRow/StepRow.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useRef, useState } from 'react';
+import classNames from 'classnames';
 import H3 from '../../../H3';
 
 function StepRow({ value }) {
+  const [focus, setFocus] = useState(false);
+  const textContainerRef = useRef(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setFocus(entry.isIntersecting);
+      },
+      {
+        threshold: 0.1,
+      },
+    );
+    observer.observe(textContainerRef.current);
+    return () => observer.disconnect();
+  }, []);
+  const textContainerStyle = [
+    'flex-1 flex flex-col gap-3 duration-700',
+    { '-translate-y-20 opacity-0': !focus },
+    { 'translate-y-0 opacity-100': focus },
+  ];
+
   const {
     id, title, content, pic,
   } = value;
@@ -11,7 +33,7 @@ function StepRow({ value }) {
                  max-xl:gap-[5vw]
                  max-sm:flex-col max-sm:even:flex-col'
     >
-      <div className='flex-1 flex flex-col gap-3'>
+      <div ref={textContainerRef} className={classNames(textContainerStyle)}>
         <div className='text-[5.375rem] font-semibold text-grey-tint'>{id}</div>
         <H3>{title}</H3>
         <p className='text-lg mt-5'>{content}</p>
